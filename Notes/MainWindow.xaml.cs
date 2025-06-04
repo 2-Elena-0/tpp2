b581f54d74678fee61b84590bd0e1025f994de11
2-Elena-0 <e.nowitskaya2017@yandex.ru>
2025-06-04 14:29:56 +0300

    Добавление заметок

---
+++ Notes/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] notes = new string[100];
+        public MainWindow()
+        {
+            GetNotes();
+            InitializeComponent();
+            foreach (string note in notes)
+            {
+                NoteList.Items.Add(note);
+            }
+        }
+        private void GetNotes()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("../../../notes");
+                string line = sr.ReadLine();
+                int i = 0;
+                while (line != null)
+                {
+                    notes[i++] = line;
+                    line = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+    }
+}
